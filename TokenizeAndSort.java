14badeb1beab034e7b0530630f7d775255a494ba
Hemachandru <2006hemachandru@gmail.com>
2026-08-20 10:27:45 +0530

    Update TokenizeAndSort.java

--- TokenizeAndSort.java
+++ TokenizeAndSort.java
@@ -1,27 +1,54 @@
-import java.util.Scanner;
-import java.util.TreeSet;
-public class TokenizeAndSort
-{
-public static void main(String[] args)
-{
+public class SelectionSort {
 
+    public static <T extends Comparable<T>> void SelectionSort(T[] array) {
 
+        int n = array.length;
 
+        for (int i = 0; i < n - 1; i++) {
 
+            int minIndex = i;
 
+            for (int j = i + 1; j < n; j++) {
 
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
 
+            T temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
 
+    public static void main(String[] args) {
 
+        Integer[] intArray = {5, 3, 9, 1, 7};
 
+        Float[] floatArray = {5.5f, 3.3f, 9.9f, 1.1f, 7.7f};
 
+        System.out.println("Original Integer Array:");
+        printArray(intArray);
 
+        System.out.println("Original Float Array:");
+        printArray(floatArray);
 
+        SelectionSort(intArray);
+        SelectionSort(floatArray);
 
+        System.out.println("Sorted Integer Array:");
+        printArray(intArray);
 
+        System.out.println("Sorted Float Array:");
+        printArray(floatArray);
+    }
 
+    public static <T> void printArray(T[] array) {
 
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
 
-
-}
+        System.out.println();
+    }
 }
